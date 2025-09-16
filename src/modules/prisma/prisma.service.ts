4f665cef8067d98f9f357eaaf3df8ac829432d83
Bruno Raposo <brunoraposounb@gmail.com>
2025-09-16 16:41:28 -0300

    Feat: prisma config

---
+++ src/modules/prisma/prisma.service.ts
@@ -0,0 +1,16 @@
+import { Injectable, OnModuleInit, OnApplicationShutdown } from "@nestjs/common";
+import { PrismaClient } from "@prisma/client";
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    super();
+  }
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async OnApplicationShutdown() {
+    await this.$disconnect();
+  }
+}
