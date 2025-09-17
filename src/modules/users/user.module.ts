413d1bbbdf2debf4e2dc702669852bb73a91c273
Bruno Raposo <brunoraposounb@gmail.com>
2025-09-17 16:08:30 -0300

    Feat: Add user routes

---
+++ src/modules/users/user.module.ts
@@ -0,0 +1,12 @@
+import { UserService } from './user.service';
+import { UserController } from './user.controller';
+import { Module } from '@nestjs/common';
+import { PrismaModule } from '../prisma/prisma.module';
+
+@Module({
+  imports: [PrismaModule],
+  controllers: [UserController],
+  providers: [UserService],
+  exports: [UserService],
+})
+export class UserModule {}
