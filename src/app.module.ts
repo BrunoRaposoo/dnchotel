9e30c4a03768b1b36a5fe82bf35783645c4a4ca7
Bruno Raposo <brunoraposounb@gmail.com>
2025-09-25 15:49:28 -0300

    Feat: add jwt token for login

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
+import { AuthModule } from './modules/auth/auth.module';
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './modules/prisma/prisma.module';
 import { UserModule } from './modules/users/user.module';
 
 @Module({
-  imports: [PrismaModule, UserModule],
+  imports: [AuthModule, PrismaModule, UserModule],
 })
 export class AppModule {}
---
+++ src/modules/auth/auth.service.ts
@@ -0,0 +1,33 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { User } from '@prisma/client';
+import { AuthLoginDTO } from './domain/dto/authLogin.dto';
+import { PrismaService } from '../prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly prisma: PrismaService,
+  ) {}
+
+  async generateJwtToken(user: User) {
+    const payload = { sub: user.id, name: user.name };
+    const options = {
+      expiresIn: '1d',
+      issuer: 'dnc_hotel',
+      audience: 'users',
+    };
+    return { access_token: this.jwtService.sign(payload, options) };
+  }
+
+  async login({ email, password }: AuthLoginDTO) {
+    const user = await this.prisma.user.findUnique({ where: { email } });
+    if (!user || (await bcrypt.compare(password, user.password))) {
+      throw new UnauthorizedException('Email or password is incorrect');
+    }
+
+    return await this.generateJwtToken(user)
+  }
+}
