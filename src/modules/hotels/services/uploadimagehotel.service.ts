9f26f23a8eea3578b6aa3b9d636826aa8621848e
Bruno Raposo <brunoraposounb@gmail.com>
2026-03-06 14:18:34 -0300

    FEAT: remove cache from redis

--- src/modules/hotels/services/uploadimagehotel.service.ts
+++ src/modules/hotels/services/uploadimagehotel.service.ts
@@ -3,12 +3,16 @@ import { REPOSITORY_TOKEN_HOTEL } from '../utils/repositoriesToken';
 import type { IHotelRepository } from '../domain/repositories/IHotel.repositories';
 import { join, resolve } from 'path';
 import { unlink } from 'fs/promises';
+import { InjectRedis } from '@nestjs-modules/ioredis';
+import { Redis } from 'ioredis';
+import { REDIS_HOTEL_KEY } from '../utils/redisKey';
 
 @Injectable()
 export class UploadImageHotelService {
   constructor(
     @Inject(REPOSITORY_TOKEN_HOTEL)
     private readonly hotelRepositories: IHotelRepository,
+    @InjectRedis() private readonly redis: Redis,
   ) {}
 
   async execute(id: string, imageFileName: string) {
@@ -36,6 +40,8 @@ export class UploadImageHotelService {
         }
       }
     }
+
+    await this.redis.del(REDIS_HOTEL_KEY);
 
     return await this.hotelRepositories.updateHotel(Number(id), {
       image: imageFileName,
